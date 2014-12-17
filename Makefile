ee05c3a81214bbaed43f6152dd922ba45523ddd8
Razvan Marinescu <rvm10@imperial.ac.uk>
2014-12-17 23:46:53 +0000

    finished the latex figure, added makefile and all the results .. have a good night

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	python recolor_eps_raz.py --mapFile input/labels_raz.xml mid_prep.eps ordering_figures_final/report1_gen.tex
--- recolor_eps_raz.py
+++ recolor_eps_raz.py
@@ -86,18 +86,18 @@ if options.verbose:
 # 1) Prepare an eps file to be recolored by identifying the items in
 #    the eps file that are colored, or
 # 2) Recolor a prepared eps file given a color mapping file.
-# 
-# To prepare an eps file, first "eps2eps" is run on it to remove the 
+#
+# To prepare an eps file, first "eps2eps" is run on it to remove the
 # cruft.  Then the file is written out for each colored item found with
 # that colored item set to a given color (yellow by default), opened
 # for viewing, and then asks for an identifier for that colored item.
 # Once all desired colored items are identified, the eps file is written
 # out with a comment indicating the identifier for each color.
-# 
+#
 # To recolor the prepared eps file, a mapping file is read that gives
 # the color for each of the identifiers that might be found in the
 # eps file comments.  The mapping file is XML like this:
-# 
+#
 # 	<?xml version="1.0"?>
 # 	<LabelList>
 #	<Label>
@@ -140,101 +140,252 @@ NUMS_TO_EVENT = {170:6, 171:6, 132:11, 133:11, 154:18, 155:18, 106:19, 107:19, 2
 CSV_MATRICES = ['EBM_matrix.csv', 'GMM_matrix.csv', 'cluster1_matrix.csv', 'cluster2_matrix.csv', 'cluster3_matrix.csv']
 OUT_FOLDERS = [ "ordering_figures_final/images/%s" % x.split("_")[0] for x in CSV_MATRICES]
 
+
+# 1st_ball=ABETA(event at index 0) 2nd_ball=PTAU(index 0) ... 7th_ball=FDG(index 12)
+BALL_ABN_IND = [0,1,2,3,4,5,12]
+
+NR_STAGES = len(stageIndexAbnormal)
+NR_MATRICES = len(CSV_MATRICES)
+NR_EVENTS = 42
+NR_BALLS = 7
+
+
 def getInterpolatedColor(abn_level):
   # abn_level = 0 -> yellow
   # abn_level = 1 -> red
   print "abn_level: %f" % abn_level
   x = int((1-abn_level)*255)
-  print "x:%f" % x 
+  print "x:%f" % x
   return "255 %d 0" % x
 
+def create_images():
+  if os.access(options.mapFileName,os.F_OK): # then file exists
+    # Read color map file
+    xmldoc = minidom.parse(options.mapFileName)
+    xmlLabelList = xmldoc.getElementsByTagName("Label")
+    for xmlLabel in xmlLabelList:
+      name = xmlLabel.getElementsByTagName("Name")
+      labelList.append(name[0].firstChild.data)
+      uNumber = xmlLabel.getElementsByTagName("Number")
+      number = int(uNumber[0].firstChild.data)
+      labelNums.append(number)
 
-NR_STAGES = len(stageIndexAbnormal)
+  eventsAbnormalityAll = np.zeros([NR_MATRICES, NR_STAGES, NR_EVENTS], float)
+  #for matrixName,outFolder in zip([CSV_MATRICES[0]], [OUT_FOLDERS[0]]):
+  for matrixIndex in range(len(CSV_MATRICES)):
+    matrixName = CSV_MATRICES[matrixIndex]
+    outFolder = OUT_FOLDERS[matrixIndex]
+    matrix = np.loadtxt(open(matrixName,"rb"),delimiter=",")
+    for stageIndex in range(NR_STAGES):
 
-if os.access(options.mapFileName,os.F_OK): # then file exists
-	# Read color map file
-	xmldoc = minidom.parse(options.mapFileName)
-	xmlLabelList = xmldoc.getElementsByTagName("Label")
-	for xmlLabel in xmlLabelList:
-		name = xmlLabel.getElementsByTagName("Name")
-		labelList.append(name[0].firstChild.data)
-		uNumber = xmlLabel.getElementsByTagName("Number")
-		number = int(uNumber[0].firstChild.data)
-		labelNums.append(number)
+      #print matrix
+      # for each event get the sum of all the probabilities until the current stage
+      eventsAbnormality = np.sum(matrix[:,:STAGE_NR_LABELS[stageIndex]],1)
+      print eventsAbnormality
 
-#for matrixName,outFolder in zip([CSV_MATRICES[0]], [OUT_FOLDERS[0]]):
-for matrixName,outFolder in zip(CSV_MATRICES, OUT_FOLDERS):
-  matrix = np.loadtxt(open(matrixName,"rb"),delimiter=",")
-  for stageIndex in range(NR_STAGES):
+      assert(len(eventsAbnormality) == NR_EVENTS)
+      eventsAbnormalityAll[matrixIndex, stageIndex, :] = eventsAbnormality
 
-    #print matrix
-    # for each event get the sum of all the probabilities until the current stage
-    eventsAbnormality = np.sum(matrix[:,:STAGE_NR_LABELS[stageIndex]],1)
-    print eventsAbnormality
-    
 
-    labelColors = []
-    redNums = ABNORMAL_ORDER[:stageIndexAbnormal[stageIndex]]
-    yellowNums = set(ABNORMAL_ORDER) - set(redNums)
-    for labelNum in labelNums:
-      if labelNum in ABNORMAL_ORDER:
-        color = getInterpolatedColor(eventsAbnormality[NUMS_TO_EVENT[labelNum]])
-        print color
-        labelColors.append(color)
-      else:
-        labelColors.append(GRAY)
-        
-    print "\nColoring file %s using matrix %s, stage %d ..." % (inFileName, matrixName, stageIndex)
+      labelColors = []
+      redNums = ABNORMAL_ORDER[:stageIndexAbnormal[stageIndex]]
+      yellowNums = set(ABNORMAL_ORDER) - set(redNums)
+      for labelNum in labelNums:
+        if labelNum in ABNORMAL_ORDER:
+          color = getInterpolatedColor(eventsAbnormality[NUMS_TO_EVENT[labelNum]])
+          print color
+          labelColors.append(color)
+        else:
+          labelColors.append(GRAY)
 
-    # Read input file and write ouput, changing the colors after
-    #  finding '% recoloreps LABELSTRING' comments
-    ff = open(inFileName,'r')
-    contents = ff.read()
-    ff.close()
-    contentLines = contents.split('\n')
+      print "\nColoring file %s using matrix %s, stage %d ..." % (inFileName, matrixName, stageIndex)
 
-    # open output file for writing
-    newOutFileName = "%s/stage_%d.eps" % (outFolder, STAGE_NR_LABELS[stageIndex])
-    print newOutFileName
-    of = open(newOutFileName,'w')
-    #of = open(os.devnull,'w')
+      # Read input file and write ouput, changing the colors after
+      #  finding '% recoloreps LABELSTRING' comments
+      ff = open(inFileName,'r')
+      contents = ff.read()
+      ff.close()
+      contentLines = contents.split('\n')
 
-    skipNextLine = False
-    for line in contentLines:
-      if skipNextLine == False:
-        h = re.compile('% recoloreps .*')
-        hS = h.search(line)
-        if hS: # if this is a color comment line
+      # open output file for writing
+      newOutFileName = "%s/stage_%d.eps" % (outFolder, STAGE_NR_LABELS[stageIndex])
+      print newOutFileName
+      of = open(newOutFileName,'w')
+      #of = open(os.devnull,'w')
+
+      skipNextLine = False
+      for line in contentLines:
+        if skipNextLine == False:
+          h = re.compile('% recoloreps .*')
+          hS = h.search(line)
+          if hS: # if this is a color comment line
+            if options.verbose:
+              print 'Found color comment: ', line
+            of.write(line+'\n')
+            toFind = line[13:]
+            if toFind in labelList:
+              if options.verbose:
+                print 'looking for color for ', toFind
+              index = labelList.index(toFind)
+              if options.verbose:
+                print 'index is ', index
+              color = labelColors[index]
+              if options.verbose:
+                print 'writing color: ', color
+              of.write(color+' rG\n')
+              skipNextLine = True
+            else:
+              if options.verbose:
+                print toFind + ' is not in labelList\n'
+          else: # something other than color, print it out
+            of.write(line+'\n')
+        else:
           if options.verbose:
-            print 'Found color comment: ', line
-          of.write(line+'\n')
-          toFind = line[13:]
-          if toFind in labelList:
-            if options.verbose:
-              print 'looking for color for ', toFind
-            index = labelList.index(toFind)
-            if options.verbose:
-              print 'index is ', index
-            color = labelColors[index]
-            if options.verbose:
-              print 'writing color: ', color
-            of.write(color+' rG\n')
-            skipNextLine = True
-          else:
-            if options.verbose:
-              print toFind + ' is not in labelList\n'
-        else: # something other than color, print it out
-          of.write(line+'\n')
-      else:
-        if options.verbose:
-          print 'skipped actual color line\n'
-        skipNextLine = False
+            print 'skipped actual color line\n'
+          skipNextLine = False
 
-    of.close()
+      of.close()
 
-# --------------------------------------------------------------------
-#
-if options.verbose:
-	print "All done, bye."
+  # --------------------------------------------------------------------
+  #
+  if options.verbose:
+    print "All done, bye."
 
-sys.exit()
+  print eventsAbnormalityAll
+  return eventsAbnormalityAll
+
+  #sys.exit()
+
+def create_latex(eventsAbnormalityAll):
+  text = r'''
+\documentclass[11pt,a4paper,oneside]{report}
+
+\usepackage{float}
+\usepackage{tikz}
+\usetikzlibrary{plotmarks}
+\usepackage{amsmath,graphicx}
+\usepackage{epstopdf}
+\usepackage[font=normal,labelfont=bf]{caption}
+\usepackage{subcaption}
+\usepackage{color}
+
+
+% margin size
+\usepackage[margin=1in]{geometry}
+
+% tikz settings
+\tikzstyle{state}=[circle,thick,draw=black, align=center, minimum size=2.1cm,
+inner sep=0]
+\tikzstyle{vertex}=[circle,thick,draw=black]
+\tikzstyle{terminal}=[rectangle,thick,draw=black]
+\tikzstyle{edge} = [draw,thick]
+\tikzstyle{lo} = [edge,dotted]
+\tikzstyle{hi} = [edge]
+\tikzstyle{trans} = [edge,->]
+
+
+\begin{document}
+\belowdisplayskip=12pt plus 3pt minus 9pt
+\belowdisplayshortskip=7pt plus 3pt minus 4pt
+\tikzset{every picture/.append style={scale=0.6}}
+\newcommand*{\scaleBrainImg}{0.3}
+
+%col{x}{y}{z} respresents the color for ball z from matrix x at stage y (matrix x, stage y, ball z)
+'''
+
+  for matrixIndex in range(NR_MATRICES):
+    for stageIndex in range(NR_STAGES):
+      for ballIndex in range(NR_BALLS):
+        # abnormality=0 -> yellow (greenComponent=1); abnormality=1 -> red (greenComponent=0)
+        greenVal =  1 - eventsAbnormalityAll[matrixIndex, stageIndex, BALL_ABN_IND[ballIndex]]
+        text += r'''
+\definecolor{col''' + "%d%d%d" % (matrixIndex, stageIndex, ballIndex) + '''}{rgb}{1,''' + str(greenVal) + ''',0}'''
+
+  text += '''\n\n '''
+  for matrixIndex in range(NR_MATRICES):
+    text += r'''
+\begin{figure}[H]
+  \centering'''
+    for stageIndex in range(NR_STAGES):
+      text += r'''
+  %\begin{subfigure}[b]{0.15\textwidth}
+    \begin{tikzpicture}[scale=1.0,auto,swap]
+
+    % the two brain figures on top
+    \node (upper_brain) at (0,1.5) { \includegraphics*[scale=\scaleBrainImg,trim=0 0 240 0]{'''
+
+      text +=  "images/%s/stage_%d.eps" % (CSV_MATRICES[matrixIndex].split("_")[0], STAGE_NR_LABELS[stageIndex]) + r'''}};
+    \node (lower_brain) at (0,-1.5) { \includegraphics*[scale=\scaleBrainImg,trim=240 0 0 0]{'''
+      text +=  "images/%s/stage_%d.eps" % (CSV_MATRICES[matrixIndex].split("_")[0], STAGE_NR_LABELS[stageIndex]) + r'''}};
+
+    % the 6 dots
+    \draw[fill=''' + "col%d%d0" % (matrixIndex, stageIndex) + '''] (-1.6,-3.4) ellipse (0.3cm and 0.3cm);
+    \draw[fill=''' + "col%d%d1" % (matrixIndex, stageIndex) + '''] (-0.7,-3.4) ellipse (0.3cm and 0.3cm);
+    \draw[fill=''' + "col%d%d2" % (matrixIndex, stageIndex) + '''] (0.2,-3.4) ellipse (0.3cm and 0.3cm);
+    \draw[fill=''' + "col%d%d3" % (matrixIndex, stageIndex) + '''] (-1.6,-4.1) ellipse (0.3cm and 0.3cm);
+    \draw[fill=''' + "col%d%d4" % (matrixIndex, stageIndex) + '''] (-0.7,-4.1) ellipse (0.3cm and 0.3cm);
+    \draw[fill=''' + "col%d%d5" % (matrixIndex, stageIndex) + '''] (0.2,-4.1) ellipse (0.3cm and 0.3cm);
+
+    % the big dot on the right
+    \draw[fill=''' + "col%d%d6" % (matrixIndex, stageIndex) + '''] (1.3,-3.75) ellipse (0.6cm and 0.6cm);
+
+    \end{tikzpicture}
+  %\end{subfigure}
+  % next subfigure
+  \hspace{-1.5em}
+  ~'''
+
+    text += r'''
+  \hspace{1em}
+  \begin{tikzpicture}[scale=1.0,auto,swap]
+    \shade[top color=yellow,bottom color=red] (0,0) rectangle (0.5,6);
+    \node[inner sep=0] (corr_text) at (0.2,6.5) {0};
+    \node[inner sep=0] (corr_text) at (0.2,-0.5) {1};
+  \end{tikzpicture}
+  \caption{''' + CSV_MATRICES[matrixIndex].split("_")[0] +'''}
+\end{figure}
+
+'''
+  text += r'''
+
+\newcommand*{\scaleLabelImg}{1}
+\begin{figure}[H]
+
+  \begin{tikzpicture}[scale=1.0,auto,swap]
+
+  % the two brain figures on top
+  \node (brain) at (0,1.5) { \includegraphics*[scale=\scaleLabelImg]{images/Mid-Lateral_surface3.eps}};
+
+  % the 6 dots
+  \draw[scale=\scaleLabelImg] (-11,-6) circle [radius=2cm] node {ABETA142};
+  \draw[scale=\scaleLabelImg] (-6,-6) circle [radius=2cm] node {PTAU181P};
+  \draw[scale=\scaleLabelImg] (-1,-6) circle [radius=2cm] node {TAU};
+  \draw[scale=\scaleLabelImg] (-11,-10.5) circle [radius=2cm] node {RAVLT};
+  \draw[scale=\scaleLabelImg] (-6,-10.5) circle [radius=2cm] node {MMSE};
+  \draw[scale=\scaleLabelImg] (-1,-10.5) circle [radius=2cm] node {ADAS13};
+
+  % the big dot on the right
+  \draw[scale=\scaleLabelImg] (6,-8.25) circle [radius=4cm] node {FDG-PET};
+
+  \end{tikzpicture}
+
+  \caption{Labels of the different areas analysed}
+\end{figure}
+
+\end{document}
+
+'''
+
+  return text
+
+
+
+
+eventsAbnormalityAll = create_images()
+text = create_latex(eventsAbnormalityAll)
+
+out = open(outFileName, 'w')
+out.write(text)
+out.close()
+os.system("cd ordering_figures_final && xelatex report1_gen.tex")
+print text
